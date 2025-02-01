8a3547da10d352b589c83603a2997d774aef0f33
fellypeavelino <fellypeavelino@hotmail.com>
2025-02-01 17:53:51 -0300

    criando intercept para token

---
+++ src/main/java/com/teste/noxtec/noxtec/config/AuthInterceptor.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.teste.noxtec.noxtec.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+/**
+ *
+ * @author Usuario
+ */
+@Component
+public class AuthInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || authHeader.isEmpty()) {
+            System.out.println("Authorization: "+authHeader);
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Authorization header is missing");
+            return false;
+        }
+        return true;
+    }
+}
